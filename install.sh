517ab2825423d558cdb00ea3b7b608e23e92e888
Garrett Holmstrom <gholmstrom@zoox.com>
2025-01-17 16:44:44 -0800

    Switch to ssh_config's Include directive

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@
 install -d -m 0700 ~/.ssh/sockets
 install -d -m 0700 ~/tmp
 
-# Combine a bunch of ssh config files
-# OpenSSH 7.3 adds an "Include" directive for this, so I can start
-# using that when I'm done with RHEL 7.
-chmod 0700 dotfiles/.ssh
-dotfiles/.ssh/generate-config
-
 # Have to pattern match because * won't find dotfiles
 rsync -lpqrt dotfiles/ ~/
 
