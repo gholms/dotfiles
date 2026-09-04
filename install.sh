23d1e5d4d239a042a9760c1d6528c68da52df850
Garrett Holmstrom <gholmstrom@zoox.com>
2026-09-04 12:37:50 -0700

    git:  set core.untrackedCache

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 install -d -m 0700 ~/.ssh/sockets
 install -d -m 0700 ~/tmp
 
+test -e ~/.gitconfig || cp -p dotfiles/.gitconfig ~/.gitconfig
+rm -f ~/.pythonrc
 # Have to pattern match because * won't find dotfiles
-rsync -lpqrt dotfiles/ ~/
+rsync -lpqrt --exclude .gitconfig dotfiles/ ~/
 
 crontab crontab
